b2b4cb61ed1999a278726f20240aefe16c0c56b7
hirosof <for-git@mail.hirosof.dev>
2026-06-09 06:09:23 +0900

    dotnet_legacyのサンプルを追加

---
+++ samples/white/pe/dotnet_legacy/dotnet481_console/dotnet481_console/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet481_console {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("これはFileStaticAnalyzerStudio向けのサンプルEXEファイル(.NET Framework)です。");
+            Console.WriteLine("ターゲット .NET Framework バージョン：4.8.1");
+        }
+    }
+}
